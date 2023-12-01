6d06d38eb0af89a16ce1a34507f68aed1bb89aea
jappelgren <justinappelgren@gmail.com>
2023-12-01 10:50:25 -0600

    added second star test, so much for tdd

--- day-01/main_test.go
+++ day-01/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -13,4 +14,17 @@ func TestFirstStar(t *testing.T) {
 	if res != want || err != nil {
 		t.Fatalf(`FirstStar(%q) should return %#v, instead received %v`, input, want, res)
 	}
+	fmt.Printf(`First Star test wanted %v, got %v. Test passed%v`, want, res, "\r\n")
+}
+
+func TestSecondStar(t *testing.T) {
+	input := "sample2.txt"
+	want := 281
+	parsed := ParseFileToBytes(input)
+	res, err := SecondStar(parsed)
+
+	if res != want || err != nil {
+		t.Fatalf(`SecondStar(%q) should return %#v, instead received %v`, input, want, res)
+	}
+	fmt.Printf(`Second Star test wanted %v, got %v. Test passed%v`, want, res, "\r\n")
 }
