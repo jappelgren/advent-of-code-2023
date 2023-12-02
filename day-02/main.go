0e7b96e6154d91a1eb8f10e261eb32793edd7676
jappelgren <justinappelgren@gmail.com>
2023-12-02 09:17:17 -0600

    added test for day 2 star 2 and finished the problem

--- day-02/main.go
+++ day-02/main.go
@@ -26,7 +26,8 @@ func FirstStar(f []string) int {
 		valid := true
 		for _, w := range split {
 			pair := regexp.MustCompile("[0-9]{2,}|[a-zA-Z]{3,5}").FindAllString(w, -1)
-			if cubes, err := strconv.Atoi(pair[0]); err == nil && ((pair[1] == "red" && cubes > maxR) || (pair[1] == "green" && cubes > maxG) || (pair[1] == "blue" && cubes > maxB)) {
+			if cubes, err := strconv.Atoi(pair[0]); err == nil &&
+				((pair[1] == "red" && cubes > maxR) || (pair[1] == "green" && cubes > maxG) || (pair[1] == "blue" && cubes > maxB)) {
 				valid = false
 				break
 			}
@@ -34,12 +35,29 @@ func FirstStar(f []string) int {
 
 		if len(split) == 0 || valid {
 			res += gameId
-		} 
+		}
 	}
 
 	return res
 }
 
 func SecondStar(f []string) int {
-	return 0
+	var res int
+
+	for _, v := range f {
+		gameless := regexp.MustCompile("Game\\s[0-9]{1,}:").ReplaceAll([]byte(v), []byte(""))
+		split := regexp.MustCompile("[0-9]{1,}\\s[a-zA-Z]{3,5}").FindAllString(string(gameless), -1)
+
+		maxMap := map[string]int{"red": 0, "blue": 0, "green": 0}
+		for _, w := range split {
+			pair := regexp.MustCompile("[0-9]{1,}|[a-zA-Z]{3,5}").FindAllString(w, -1)
+			if cubes, err := strconv.Atoi(pair[0]); err == nil && cubes > maxMap[pair[1]] {
+				maxMap[pair[1]] = cubes
+			}
+		}
+
+		res += maxMap["red"] * maxMap["blue"] * maxMap["green"]
+	}
+
+	return res
 }
--- day-02/main_test.go
+++ day-02/main_test.go
@@ -13,3 +13,13 @@ func TestFirstStar(t *testing.T) {
 		t.Fatalf(`Expected FirstStar(%v) to equal %v, instead got %v.%v`, input, want, res, "\r\n")
 	}
 }
+
+func TestSecondStar(t *testing.T) {
+	input := ParseFileToStringByNewLine("example.txt")
+	want := 2286
+	res := SecondStar(input)
+
+	if res != want {
+		t.Fatalf(`Expected SecondStar(%v) to equal %v, instead got %v.%v`, input, want, res, "\r\n")
+	}
+}
