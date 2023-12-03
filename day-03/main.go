edcb6b6826411b842324c1db5bf021e0e7404cb0
jappelgren <justinappelgren@gmail.com>
2023-12-03 13:43:00 -0600

    i got some typ of thing going for day three star 1

--- day-03/main.go
+++ day-03/main.go
@@ -1,24 +1,130 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"strconv"
+)
 
-func main (){
-	input := ParseFileToStringByNewLine("example.txt")
+const (
+	UP = iota
+	DOWN
+	LEFT
+	RIGHT
+)
+
+func main() {
+	input := ParseFileToBytes("example.txt")
 	fmt.Println("Welcome to day 3")
 
 	first := FirstStar(input)
 	fmt.Printf(`First Star result: %v%v`, first, "\r\n")
-	
+
 	second := SecondStar(input)
 	fmt.Printf(`Second Star result: %v%v`, second, "\r\n")
 
-
 }
 
-func FirstStar (f []string) int {
-	return 0
+func FirstStar(f []byte) (res int) {
+	var rowLen int
+	var noLBs []byte
+
+	for i, v := range f {
+		if v == 10 || v == 13 {
+			if rowLen == 0 {
+				rowLen = i
+			}
+			continue
+		}
+		noLBs = append(noLBs, v)
+	}
+
+	// adjacent := []int{}
+	lastByte := byte(46)
+	var numStr string
+	indexMap := map[int][]int{}
+	var tempISlice []int
+	for i, v := range f {
+		if num := v - 48; num >= 0 && num <= 9 {
+			if len(numStr) == 0 {
+				numStr = string(rune(num))
+			} else if lastByte -48 >= 0 && lastByte - 48 <= 9 {
+				numStr += string(rune(num)) 
+			}
+			tempISlice = append(tempISlice, i)
+		} else {
+			numKey, err := strconv.Atoi(numStr); if err == nil {
+				indexMap[numKey] = tempISlice
+				tempISlice = []int{}
+				numStr = ""
+			}
+		}
+		lastByte = v
+	}
+	fmt.Println(indexMap)
+
+	return
 }
 
-func SecondStar (f []string) int {
-	return 0
+func SecondStar(f []byte) (res int) {
+	res = 0
+	return
+}
+
+func FindAdjacentIndicies(i int, rowLen int, arLen int) (adjacentIndicies []int) {
+	left := dir(i, rowLen, LEFT)
+	right := dir(i, rowLen, RIGHT)
+	below := dir(i, rowLen, DOWN)
+	above := dir(i, rowLen, UP)
+	aboveleft := dir(left, rowLen, UP)
+	aboveright := dir(right, rowLen, UP)
+	belowleft := dir(left, rowLen, DOWN)
+	belowright := dir(right, rowLen, DOWN)
+
+	// first row check
+	if i < rowLen {
+		above = -1
+		aboveleft = -1
+		aboveright = -1
+	}
+	// last row check
+	if arLen-i <= rowLen {
+		below = -1
+		belowleft = -1
+		belowright = -1
+	}
+	// first col check
+	if i%rowLen == 0 {
+		left = -1
+		aboveleft = -1
+		belowleft = -1
+	}
+	// last col check
+	if i%rowLen+1 == rowLen {
+		right = -1
+		aboveright = -1
+		belowright = -1
+	}
+
+	allDirs := []int{above, aboveright, right, belowright, below, belowleft, left, aboveleft}
+	for _, v := range allDirs {
+		if v >= 0 {
+			adjacentIndicies = append(adjacentIndicies, v)
+		}
+	}
+
+	return
+}
+
+func dir(i int, rowLen int, dir int) (dirI int) {
+	switch dir {
+	case UP:
+		dirI = i - rowLen
+	case RIGHT:
+		dirI = i + 1
+	case DOWN:
+		dirI = i + rowLen
+	case LEFT:
+		dirI = i - 1
+	}
+	return
 }
--- day-03/main_test.go
+++ day-03/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestFirstStar(t *testing.T) {
-	input := ParseFileToStringByNewLine("example.txt")
-	want := 8
+	input := ParseFileToBytes("example.txt")
+	want := 4361
 	res := FirstStar(input)
 
 	if res != want {
@@ -14,12 +14,120 @@ func TestFirstStar(t *testing.T) {
 	}
 }
 
-func TestSecondStar(t *testing.T) {
-	input := ParseFileToStringByNewLine("example.txt")
-	want := 2286
-	res := SecondStar(input)
-
-	if res != want {
-		t.Fatalf(`Expected SecondStar(%v) to equal %v, instead got %v.%v`, input, want, res, "\r\n")
+func TestFindAdjacentIndicies(t *testing.T) {
+	i := 14
+	rowLen := 10
+	arrLen := 100
+	want := []int{4,5,15,25,24,23,13,3}
+	res := FindAdjacentIndicies(i, rowLen, arrLen)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
 	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 4
+	rowLen = 10
+	want = []int{5,15,14,13,3}
+	res = FindAdjacentIndicies(i, rowLen, arrLen)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 94
+	rowLen = 10
+	want = []int{84,85,95,93,83}
+	res = FindAdjacentIndicies(i, rowLen, arrLen)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 23
+	rowLen = 10
+	want = []int{13,14,24,34,33,32,22,12}
+	res = FindAdjacentIndicies(i, rowLen, arrLen)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 5
+	rowLen = 10
+	want = []int{6,4}
+	res = FindAdjacentIndicies(i, rowLen, 10)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 10
+	rowLen = 10
+	want = []int{0,1,11,21,20}
+	res = FindAdjacentIndicies(i, rowLen, 100)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
+
+	i = 19
+	rowLen = 10
+	want = []int{9,29,28,18,8}
+	res = FindAdjacentIndicies(i, rowLen, 100)
+	
+	if len(res) != len(want) {
+		t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+	}
+
+	for i, v := range res {
+		if v != want[i] {
+			t.Fatalf(`Expected FindAdjacentIndicies(%v) to equal %v, instead got %v.%v`, i, want, res, "\r\n")
+		}
+	} 
 }
+
+// func TestSecondStar(t *testing.T) {
+// 	input := ParseFileToStringByNewLine("example.txt")
+// 	want := 2286
+// 	res := SecondStar(input)
+
+// 	if res != want {
+// 		t.Fatalf(`Expected SecondStar(%v) to equal %v, instead got %v.%v`, input, want, res, "\r\n")
+// 	}
+// }
